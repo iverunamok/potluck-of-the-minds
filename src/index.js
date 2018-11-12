7d2319e5ceeef1baaed40102b5c0a69ddee77c6b
ed42311 <ed42311@gmail.com>
2018-11-11 19:39:32 -0700

    Contact Page
    Original Description: Contact form that takes the first name(required), last name, email(required), phone number, message, with a recaptcha :) and emails it to potluckOfTheMinds406@gmail.com.... in the future, this could also include a pdf download and an image download.
    
    Baseline:
    
    - [x] email(required)
    - [x] recaptcha
    - [x] body of message
    - [x] added favicon
    
    Done by opening the prefered email client as emailing directly through javascript is not reccomended and should be handled server side.  We could grab an API like sendgrid or find an alternative free option.
    
    Needed?
    
    - [ ] First name(required)
    - [ ] last name
    - [ ] phone number

--- src/index.js
+++ src/index.js
@@ -1,27 +1,38 @@
 const countDownDate = new Date('Oct 26, 2018 18:00:00').getTime();
 
 // Update the count down every 1 second
-setInterval(() => {
-  'use strict';
-  // Get todays date and time
-  let now = new Date().getTime();
+if(window.location.pathname.includes('index')) {
+  setInterval(() => {
+    'use strict';
+    // Get todays date and time
+    let now = new Date().getTime();
+  
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
+  
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // Display the result in the element with id="demo"
+    let html = `${days}d ${hours}h ${minutes}m ${seconds}s `;
+    document.getElementById('counter').innerHTML = html;
+  
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById('counter').innerHTML = 'It Now';
+    }
+  }, 1000);
+}
 
-  // Find the distance between now and the count down date
-  let distance = countDownDate - now;
+function sendMail() {
+  let send = 'mailto:potluckOfTheMinds406@gmail.com?body=' + document.getElementById('body').value
+  window.open(send);
+}
 
-  // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  let html = `${days}d ${hours}h ${minutes}m ${seconds}s `;
-  document.getElementById('counter').innerHTML = html;
-
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById('counter').innerHTML = 'It Now';
-  }
-}, 1000);
+window.onSubmit = function onSubmit(token) {
+  sendMail()
+}
