6b8fc9c48067249edb21a9fa8caccb9194e9058b
ed42311 <ed42311@gmail.com>
2018-11-26 19:38:35 -0700

    added public directory

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(port, function(){
   console.log(`running on ${port}`)
 })
-
-
