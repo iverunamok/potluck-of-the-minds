874bb5329097545d871528a51a37534498d90600
ed42311 <ed42311@gmail.com>
2018-12-05 15:46:43 -0700

    parcel tested production build locally

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const isProd = () => process.env.NODE_ENV === 'production';;
-const port = isProd ? process.env.PORT : 1234
-app.use(express.static(path.join(__dirname, 'public')))
+const isProd = () => process.env.NODE_ENV === 'production';
+const port = process.env.PORT || 1234
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.listen(port, function(){
   console.log(`running on ${port}`)
