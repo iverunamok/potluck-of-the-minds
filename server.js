e973b238fba4c4a1e2b913bb8633fa36c63ed032
ed42311 <ed42311@gmail.com>
2018-12-05 12:42:54 -0700

    set to post install, think webpack is running too soon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 const isProd = () => process.env.NODE_ENV === 'production';;
 const port = isProd ? process.env.PORT : 3000
-
+console.log(path.join(__dirname, 'public'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, function(){
